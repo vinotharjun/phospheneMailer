c3d088336d2ac88e7a61e2526af164cde5d20725
Vinod Arjun <32776658+vinotharjun@users.noreply.github.com>
2019-07-30 20:29:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const express = require("express");
-
 const hbs = require("express-handlebars");
 const nodemailer=require("nodemailer");
 var multer  = require('multer')
 const fs=require("fs")
-const AWS = require('aws-sdk');
+
 
 const path=require("path");
 const port=process.env.PORT||3000;
 const app =express();
 const url = require('url');   
+
 app.engine("handlebars",hbs())
 app.set('view engine','handlebars');
 app.use('/public',express.static(path.join(__dirname,'public')));
 
-const s3 = new AWS.S3({
-    accessKeyId: "AKIAJ56OYQ7V6HBUZXNQ",
-    secretAccessKey: "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",
-    Bucket: "resume-datasets"
-});
+
 const storage = multer.diskStorage({
     destination: './public/uploads/',
     filename: function(req, file, cb){
@@ -34,44 +30,60 @@ const storage = multer.diskStorage({
     res.render("index",{msg:req.query.valid,color:req.query.color})
   })
 
-
-  //file upload
 app.post("/send",(req,res)=>{
-  upload(req, res, (err) => {
-    if (err) throw err
-    let array=req.files;
-    for(obj in array){
-     uploadFile(array[obj]["path"],array[obj]["filename"]);
-     var string = encodeURIComponent('File uploaded successfully');
-     res.redirect('/?valid=' + string+'&&color=red')
-    }
-  })
+ upload(req,res,(err)=>{
+  if(err){
+  throw err
+     } 
+  else {
+       if(req.files == undefined){
+        console.log("no files")
+       } 
+       else {
+         var array =req.files
+         var temp=[]
+       
+         for (element in array){
+           console.log(array[element]) 
+
+           temp.push({
+             filename :array[element]["originalname"],
+             path:array[element]["path"]
+           })
+         }
+        }
+        console.log(temp)
+    
+    var transporter = nodemailer.createTransport({
+      service: 'hotmail',
+    
+      auth: {
+        user: 'jbapraveen@hotmail.com',
+        pass: 'phoenicorn123'
+      }
+    });
+    var mailOptions = {
+      from: 'jbapraveen@hotmail.com',
+      to: 'linguisticsresearch@phoenicorn.com',
+      subject: 'Research',
+      text: 'hey! ',
+      attachments:temp
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+        res.send("somthing went wrong")
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.send("mail recieved thank you for your support")
+      }
+    });
+  }
+ })
+
 })
 
-const uploadFile=(file,fullname,res)=>{
-  fs.readFile(file,(err,data)=>{
-      if (err){
-          throw err;
-      }
-      console.log(data);
-      const params={
-          Bucket:"resume-datasets",
-          Key:new Date().getTime().toString()+"-"+fullname,
-          Body:data
-      }
-      s3.upload(params,(err,data)=>{
-          if(err) {
-            var string = encodeURIComponent('Some error occured');
-            res.redirect('/?valid=' + string+'&&color=red')
-            throw err;
-          }
-          console.log("file uploaded");
-          
-      })
-  })
-
-}
   app.listen(port,()=>{
       console.log("leander ")
   })
-  //hi this is vinoth
+
